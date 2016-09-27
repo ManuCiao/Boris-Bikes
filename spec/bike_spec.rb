41b01ebf6f8374747c36f8984b3b059f8f6a07ec
ManuCiao <mn.sabatino@gmail.com>
2016-09-27 16:15:30 +0100

    Complete Step 9

---
+++ spec/bike_spec.rb
@@ -0,0 +1,10 @@
+require "DockingStation"
+
+describe "Bike" do
+  it 'should respond_to working?' do
+    good_bike = Bike.new
+    expect(good_bike.working?).to eq true
+  end
+
+
+end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,6 +2,7 @@ require "DockingStation"
 
 describe "DockingStation" do
   it 'should respond to release_bike' do
+    docking_station = DockingStation.new
     expect(docking_station).to respond_to(:release_bike)
   end
 
